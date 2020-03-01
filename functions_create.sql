6a28ff1369c121db3daa7e3d5fa67611423d83b1
t1ncker <tincker@yandex.ru>
2020-03-01 21:00:24 +0300

    Additional functions inserted

--- functions_create.sql
+++ functions_create.sql
@@ -1,45 +1,80 @@
+--Adding and updating visits
+
 CREATE OR REPLACE FUNCTION
 new_visit(
-	bracelet_id uuid,
-	start_time  timestamp,
-	end_time    timestamp
+	p_bracelet_id uuid,
+	p_start_time  timestamp
 )
 RETURNS integer AS $$
-
-	INSERT INTO visit
-	VALUES()
+	INSERT INTO visit(bracelet_id, start_time)
+	VALUES(p_bracelet_id, p_start_time)
 	RETURNING visit_id AS result;
-
 $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION
-name ()
+end_visit (
+	p_visit_id integer,
+	p_end_time timestamp
+)
+RETURNS void AS $$
+	UPDATE visit
+	SET end_time = p_end_time
+	WHERE visit_id = p_visit_id;
+$$ LANGUAGE SQL;
+
+
+--Adding and updating services
+
+CREATE OR REPLACE FUNCTION
+add_service (
+	p_servtype_id   integer,
+	p_visit_id 	integer,
+	p_start_time 	timestamp
+)
 RETURNS integer AS $$
-
-	INSERT INTO table_name
-	VALUES()
-	RETURNING AS result;
-
+	INSERT INTO service (servtype_id, visit_id, start_time)
+	VALUES(p_servtype_id, p_visit_id, p_start_time)
+	RETURNING service_id AS result;
 $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION
-name ()
+end_service (
+	p_service_id integer,
+	p_end_time   timestamp
+)
 RETURNS integer AS $$
+	UPDATE service
+	SET end_time = p_end_time
+	WHERE service_id = p_service_id;
+$$ LANGUAGE SQL;
 
-	INSERT INTO table_name
-	VALUES()
-	RETURNING AS result;
 
+--Getting data from tables
+
+CREATE OR REPLACE FUNCTION
+get_current_visit(
+	OUT p_res   refcursor,
+)
+RETURNS refcursor AS $$
+	OPEN p_res FOR
+	SELECT * FROM visit WHERE end_time IS NULL;
 $$ LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION
-name ()
-RETURNS integer AS $$
-
-	INSERT INTO table_name
-	VALUES()
-	RETURNING AS result;
-
+get_ford_mustang_visit(
+	OUT p_res   refcursor,
+)
+RETURNS refcursor AS $$
+	OPEN p_res FOR
+	SELECT ford_mustanf FROM visit WHERE ;
 $$ LANGUAGE SQL;
 
+CREATE OR REPLACE FUNCTION
+get_service(
+	OUT p_res refcursor,
+
+)
+RETURNS AS $$
+
+$$ LANGUAGE SQL;
 
